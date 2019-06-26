c9227a2337207037e48ba82de645f0e7c3779a5c
one82s <one82s@yahoo.com>
2019-06-26 12:55:16 +0800

    added server files

---
+++ server/app.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const app = express()
+const api = require('./api')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const index = require('./api/index')
+
+app.set('port', (process.env.PORT || 8081))
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.use('./api', api)
+app.use(express.static('static'))
+
+app.use(morgan('dev'))
+
+// app.use(function(req, res, next){
+//     console.log('dito ba?')
+//     const err = new Error('Not Found')
+//     err.status = 404
+//     res.json(err)
+// })
+
+
+app.use('/api', index)
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost:27017/globalmantics')
+const db = mongoose.connection
+
+db.on('error', console.error.bind(console, 'connection error: Can not connect'))
+db.once('open', function (){
+    console.log('Connected to MongoDB')
+
+    app.listen(app.get('port'),function(){
+        console.log('API  Server Listening on port '+app.get('port'))
+    })
+})
